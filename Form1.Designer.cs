fedee5a3aae2e85c8c5eb471fc873253f82cfe21
Glinsky Ivan <glinsky_ivan@mail.ru>
2021-10-20 14:22:30 +0600

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace quarterPoints
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox_chart = new System.Windows.Forms.PictureBox();
+            this.button_createPoints = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox_chart
+            // 
+            this.pictureBox_chart.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox_chart.Name = "pictureBox_chart";
+            this.pictureBox_chart.Size = new System.Drawing.Size(775, 340);
+            this.pictureBox_chart.TabIndex = 0;
+            this.pictureBox_chart.TabStop = false;
+            // 
+            // button_createPoints
+            // 
+            this.button_createPoints.Location = new System.Drawing.Point(297, 383);
+            this.button_createPoints.Name = "button_createPoints";
+            this.button_createPoints.Size = new System.Drawing.Size(168, 36);
+            this.button_createPoints.TabIndex = 1;
+            this.button_createPoints.Text = "button1";
+            this.button_createPoints.UseVisualStyleBackColor = true;
+            this.button_createPoints.Click += new System.EventHandler(this.button_createPoints_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_createPoints);
+            this.Controls.Add(this.pictureBox_chart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_chart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox_chart;
+        private System.Windows.Forms.Button button_createPoints;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace quarterPoints
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_createPoints_Click(object sender, EventArgs e)
+        {
+            Graphics graphics = pictureBox_chart.CreateGraphics();
+            Pen pen = new Pen(Color.Red, 2);
+
+            // Размеры области построения графика
+            int width = pictureBox_chart.Width;
+            int height = pictureBox_chart.Height;
+
+            // Координатные оси
+            graphics.DrawLine(pen, width / 2, 0, width / 2, height);
+            graphics.DrawLine(pen, 0, height / 2, width, height / 2);
+
+            // Стрелки координатных осей
+            graphics.DrawLine(pen, width / 2, 0, width / 2 + 5, 10);
+            graphics.DrawLine(pen, width / 2, 0, width / 2 - 5, 10);
+            graphics.DrawLine(pen, width, height / 2, width - 10, height / 2 + 5);
+            graphics.DrawLine(pen, width, height / 2, width - 10, height / 2 - 5);
+
+            // Подписи координатных осей
+            graphics.DrawString("Y", new Font("10_IC_1", 12), Brushes.Black, width / 2 + 10, -5);
+            graphics.DrawString("X", new Font("10_IC_1", 12), Brushes.Black, width - 15, height / 2 + 10); //width, height / 2
+
+
+            // Координатная сетка и отметки на координатных осях
+            int lx = -width / 40 + 1;
+            for (int i = 20; i < width - 20; i += 20)
+            {
+                if (lx != 0)
+                {
+                    graphics.DrawLine(new Pen(Color.Gray, 1), 0 + i + 10, 0, 0 + i + 10, height); // Горизонтальная линия сетки
+                    graphics.DrawString(lx.ToString(), new Font("10_IC_1", 8, FontStyle.Bold), Brushes.Black, 0 + i + 5, height / 2 + 5); // подписи меток
+                }
+                graphics.DrawLine(new Pen(Color.Black, 2), 0 + i + 10, height / 2 - 5, 0 + i + 10, height / 2 + 5); // метка на оси OX
+
+                ++lx;
+            }
+
+            int ly = height / 40 - 1;
+            for (int i = 20; i < height; i += 20)
+            {
+                if (ly != 0)
+                {
+                    graphics.DrawLine(new Pen(Color.Gray, 1), 0, 0 + i, width, 0 + i); // Вертикальная линия сетки
+                    graphics.DrawString(ly.ToString(), new Font("10_IC_1", 8, FontStyle.Bold), Brushes.Black, width / 2 + 5, 0 + i - 5); // подписи меток
+                }
+                graphics.DrawLine(new Pen(Color.Black, 2), width / 2 - 5, 0 + i, width / 2 + 5, 0 + i); // метка на оси OY
+
+                --ly;
+            }
+        }
+    }
+}
